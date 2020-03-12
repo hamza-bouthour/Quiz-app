6500f9d4ef4a6bb629ca2e2c4db75b745ea58d2d
Hamza Bouthour <h.bouthour@gmail.com>
2020-03-11 22:46:14 -0700

    index.html

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
 
-$(document).ready(function () {
+const data = [
+    {
+        question: 'What colour does Mars appear in the night sky?',
+        answers: ['Yellow', 'Red', 'Green', 'Pink']
+    },
+    {
+        question: 'What are Saturn\'\s rings mostly made of?',
+        answers: ['Sequins', 'Ice, dust and rock', 'Gases', 'None of these']
+    },
+    {
+        question: 'About how old is the Sun?',
+        answers: ['7.5 trillion years old', '12.7 million years old', '4.5 billion years old', '100 million years old']
 
-    $(function startQuiz() {
-        $('.start-quiz-button').click(function () {
-            const questions = {
-                question1: ['What colour does Mars appear in the night sky?'],
-                question2: ['What are Saturn\'\s rings mostly made of?'],
-                question3: ['About how old is the Sun?'],
-                question4: ['Which of the statements below is correct?'],
-                question5: ['Which year did humans first land on the Moon?'],
-                question6: ['When an astronaut is in the space shuttle can they go out of the space shuttle?'],
-                question7: ['Who was the first person to walk on the Moon?'],
-                question8: [],
-                question9: [],
-                question10: [],
-            }
-            const answers = {
-                answer1: ['Yellow', 'Red', 'Green', 'Pink'],
-                answer2: ['Sequins', 'Ice, dust and rock', 'Gases', 'None of these'],
-                answer3: ['7.5 trillion years old', '12.7 million years old', '4.5 billion years old', '100 million years old'],
-                answer4: ['The Sun orbits the Earth', 'Our galaxy doesn\'\t move in space at all', 'Our galaxy moves through space', 'Our galaxy only spins around in the same spot'],
-                answer5: ['1965', '1971', '1960', '1969'],
-                answer6: ['Yes, if work needs to be done outside.', 'Only the commander can go out of the space shuttle.', 'Yes, but if you do, you will get sucked into the nearest blackhole.', 'No, never.'],
-                answer7: ['Buzz Aldrin', 'Michael Collins', 'Elvis Presley', 'Neil Armstrong'],
-                answer8: [],
-                answer9: [],
-                answer10: []
-            }
-            event.preventDefault();
-            $('.quiz-introduction').fadeOut(300);
-            $('.hide-form').removeClass('hide-form').addClass('question-form');
-            $('#question-number').html('1');
-            $('#remaining-question-number').html('9');
-            $('#question').html(questions.question1);
-            $('#answer1').html(answers.answer1[0]);
-            $('#answer2').html(answers.answer1[1]);
-            $('#answer3').html(answers.answer1[2]);
-            $('#answer4').html(answers.answer1[3]);
-        });
+    }
+]
+
+// // function renderQa(qa) {
+// //     const questionComponents = qa.answers.map(answer => {
+// //         return `<input type="checkbox" name="checkbox">
+// //             <label for="answer-a">${answer}</label>`
+// //     });
+
+// //     questionComponents.unshift(`<p>${qa.question}</p>`);
+
+// //     $('div.question-container div.question').html(questionComponents.join(''));
+// }
+
+function renderQa(qa) {
+    const questionComponents = qa.answers.map(element => {
+        return `<input type="checkbox" name="answer-a" id="checkbox-1">
+        <label for="answer-a" id="answer1">${element}</label><br>`
+    });
+    questionComponents.unshift(`<p id="question">${qa.question}</p>`);
+    questionComponents.unshift(`<h3 class="question-header">Question : <span id="question-number"></span></h3>`);
+    questionComponents.push(`<h4 class="remaining-question">Remaining questions : <span id="remaining-question-number"></span></h4>`);
+    questionComponents.push(`<button type="button" id="submit-answer">Submit</button>`);
+
+    $('form').html(questionComponents);
+}
+
+
+$(document).ready(function () {
+    $('.start-quiz-button').on('click', function () {
+
+
+        event.preventDefault();
+        $('.quiz-introduction').hide();
+        $('.hide-form').removeClass('hide-form').addClass('question-form');
+        $('#question-number').html('1');
+        $('#remaining-question-number').html('9');
+        renderQa(data[0]);
     });
 
+
+
+    // $('.start-quiz-button').on('click', function () {
+    //     $('div.quiz-introduction').hide();
+    //     $('#form1').addClass('question-form');
+    //     renderQa(data[index + 1]);
+    // });
+    // let countButton = document.getElementById('remaining-question-number');
+    // let count = 9;
+
+    // $('#submit-answer').on('click', function () {
+    //     count--;
+    //     document.getElementById(remaining - question - number) = count;
+
+    // });
 });
